d8f4538d11a1c3c7340e78f60389d90d6e7bd5c7
James Schultz <ninjames101@gmail.com>
2018-03-12 14:15:44 -0600

    updated instructions on JS

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+// getelementsbytagname h1 and img
 var heading = document.getElementsByTagName('h1')
-console.log(heading)
+console.log(heading);
+
+// getelementbyid img
+
+// querrySelector li, h1
+
+// querrySelectorAll h1
+
+// getelementsByClassName li
