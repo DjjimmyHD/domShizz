9248ad9084e040754d2bf37ce1d1b56fba444656
James Schultz <ninjames101@gmail.com>
2018-03-13 13:41:00 -0600

    added a lot more DOM practice, new shixx incoming

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-// getelementsbytagname h1 and img
-var heading = document.getElementsByTagName('h1')
-console.log(heading);
+// Create 2 variables and use getElementsByTagName to select the h1 and img tags.
+// Log the variables to the console, observe the results.
 
-// getelementbyid img
 
-// querrySelector li, h1
+// Create a variable and use getElementById to select the img tag.
+// Log the variable to the console, observe the result.
 
-// querrySelectorAll h1
 
-// getelementsByClassName li
+// Create a variable and use getElementsByClassName to select the li tag.
+// Log the variable to the console, observe the result.
+
+
+// Create at least 3 variables and use querySelector to select the h1, img, and li tags.
+// Try grabbing by class and id and tag name.
+// Log the variables to the console, observe the results.
+
+
+// Create at least 3 variables and use querySelectorAll to select the h1, img, and li tags.
+// Try grabbing by class and id and tag name.
+// Log the variables to the console, observe the results.
+
+// PART TWO
+// The time of the bears ruling things is over. Lets use DOM manipulation to give power
+// to our true overlords CORGIS
+
+// Lets start by changing the text of the H1 to read "CORGIS!"
+// Don't forget to select the h1 before trying to change the text
+
+
+// Do the same thing with the paragraph tag. It should read
+// "Two types of corgi lords Pembroke and Cardigan"
+
+// Cool we are on our way to spreading the good news about our new
+// overlords but the site needs more work
+
+// Lets change the image source to be a picture of a corgi of your choosing
+
+// Lets change the class of the lis to be "corgitype"
+
+// Now set the text of each li to be Pembroke and Cardigan
+
+// Change the color of the H1 to be your favorite color
+
+// STRETCH IT OUT
+
+//  Use this portion to cretae more images of corgis or more corgi 
+// related things. Maybe you want to have a collab take over and add
+// cats or brids also taking over. Use this section to create new elements
+// And add then to our corgi page
